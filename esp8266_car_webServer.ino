5a05348740bd71c43a7e5bdaf3bf76402ed9ff79
Jack LI <liyinchi@qq.com>
2024-01-20 23:49:10 +0800

    Update esp8266_car_webServer.ino
    长按和松开

--- esp8266_car_webServer.ino
+++ esp8266_car_webServer.ino
@@ -5,16 +5,16 @@
 // const char* ssid = "302"; // WiFi账号
 // const char* password = "123456781"; // WiFi密码
 
-const char *ssid = "遥控小车";     // 你想要的热点名称
-const char *password = "12345678"; // 热点的密码，至少8个字符
+const char *ssid = "遥控小车";      // 你想要的热点名称
+const char *password = "12345678";  // 热点的密码，至少8个字符
 
 // 定义L298N电机驱动板的引脚
-const int motorLeftIn1 = D1;  // 左电机输入1
-const int motorLeftIn2 = D2;  // 左电机输入2
-const int motorRightIn1 = D3; // 右电机输入1
-const int motorRightIn2 = D4; // 右电机输入2
-const int motorLeftEn = D5;   // 左电机使能
-const int motorRightEn = D6;  // 右电机使能
+const int motorLeftIn1 = D1;   // 左电机输入1
+const int motorLeftIn2 = D2;   // 左电机输入2
+const int motorRightIn1 = D3;  // 右电机输入1
+const int motorRightIn2 = D4;  // 右电机输入2
+const int motorLeftEn = D5;    // 左电机使能
+const int motorRightEn = D6;   // 右电机使能
 
 // 前端HTML页面内容定义为字符串
 const char *webpage PROGMEM = R"rawliteral(
@@ -64,43 +64,58 @@ const char *webpage PROGMEM = R"rawliteral(
   }
 </style>
 <script>
-function sendCommand(action) {
-  fetch('/goTo',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({action: action})
-    })
-    .then(response => response.text())
-    .catch(error => console.error('Error:', error));
+// JavaScript代码
+function startCommand(action) {
+  // 发送开始运动的指令到Arduino
+  fetch('/start', {
+    method: 'POST',
+    body: JSON.stringify({ action: action }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const leftBtn = document.getElementById('left');
-  const rightBtn = document.getElementById('right');
-  const forwardBtn = document.getElementById('forward');
-  const backwardBtn = document.getElementById('backward'); // 获取后退按钮
-  const stopBtn = document.getElementById('stop');
+function stopCommand() {
+  // 发送停止运动的指令到Arduino
+  fetch('/stop', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
 
-  leftBtn.addEventListener('click', () => sendCommand(1));
-  rightBtn.addEventListener('click', () => sendCommand(2));
-  forwardBtn.addEventListener('click', () => sendCommand(3));
-  backwardBtn.addEventListener('click', () => sendCommand(5)); // 后退按钮发送5作为动作
-  stopBtn.addEventListener('click', () => sendCommand(4));
+// 修改事件监听器来处理按下和松开
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => {
+    // 鼠标事件
+    button.addEventListener('mousedown', () => startCommand(button.dataset.action));
+    button.addEventListener('mouseup', stopCommand);
+    button.addEventListener('mouseleave', stopCommand);
+    // 触摸事件
+    button.addEventListener('touchstart', (e) => {
+      e.preventDefault(); // 阻止触摸时屏幕的滚动
+      startCommand(button.dataset.action);
+    });
+    button.addEventListener('touchend', stopCommand);
+  });
 });
 </script>
 </head>
 <body>
 <div id="control-panel">
-  <button id="forward" class="blue-button">前</button>
-<div id="horizontal-controls">
-  <button id="left" class="blue-button">左</button>
-  <button id="right" class="blue-button">右</button>
+  <button id="forward" class="blue-button" data-action="3">前</button>
+  <div id="horizontal-controls">
+    <button id="left" class="blue-button" data-action="1">左</button>
+    <button id="right" class="blue-button" data-action="2">右</button>
+  </div>
+  <button id="backward" class="blue-button" data-action="5">后</button>
 </div>
-<button id="backward" class="blue-button">后</button>
-<button id="stop" class="red-button">STOP</button>
+<!-- MJPEG流 -->
+<div style="margin-top: 20px;">
+  // <img style="display: block; -webkit-user-select: none; margin: auto; background-color: hsl(0, 0%, 25%);" src="http://192.168.4.2/mjpeg/1">
 </div>
 </body>
 </html>
@@ -108,10 +123,43 @@ document.addEventListener('DOMContentLoaded', function() {
 
 ESP8266WebServer server(80);
 
-void setup()
-{
-  Serial.begin(115200);
+// 函数原型声明
+void setMotorState(int action, bool continuous);
+void handleGet();
+void handlePost();
 
+// 停止所有电机
+void stopMotors() {
+  digitalWrite(motorLeftIn1, LOW);
+  digitalWrite(motorLeftIn2, LOW);
+  digitalWrite(motorRightIn1, LOW);
+  digitalWrite(motorRightIn2, LOW);
+}
+
+// 处理开始运动的请求
+void handleStart() {
+  if (server.method() == HTTP_POST) {
+    StaticJsonDocument<200> doc;
+    deserializeJson(doc, server.arg("plain"));
+    int action = doc["action"].as<int>();
+    setMotorState(action, true);  // 开始运动
+    server.send(200, "text/plain", "Start action received.");
+  }
+}
+
+// 处理停止运动的请求
+void handleStop() {
+  if (server.method() == HTTP_POST) {
+    stopMotors();  // 停止所有电机
+    server.send(200, "text/plain", "Stop action received.");
+  }
+}
+
+
+
+// 初始化
+void setup() {
+  Serial.begin(115200);
   // 连接WiFi
   // WiFi.begin(ssid, password);
   // Serial.println("Attempting to connect to WiFi network...");
@@ -121,13 +169,21 @@ void setup()
   //   Serial.print(".");
   // }
 
+  bool ssid_hidden = false; // 设置为true可以隐藏SSID
+  int channel = 1;          // 设置WiFi信道
+  int max_connection = 4;   // 设置最大连接数
   // 设置ESP8266为AP模式，使用上面定义的SSID和密码
-  WiFi.softAP(ssid, password);
+  bool result = WiFi.softAP(ssid, password, channel, ssid_hidden, max_connection);
   Serial.println("ESP8266 is set as a WiFi Access Point with SSID: " + String(ssid));
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  if(result) {
+    Serial.println("Access Point Started");
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+  } else {
+    Serial.println("Access Point Failed to Start");
+  }
 
   // 设置电机驱动引脚为输出模式
   pinMode(motorLeftIn1, OUTPUT);
@@ -143,72 +199,74 @@ void setup()
 
   // 初始化Web服务器并设置主页响应
   server.on("/", handleGet);
-  server.on("/goTo", handlePost);
+  server.on("/command", handlePost);  // 短按命令
+  server.on("/start", handleStart);   // 开始运动的请求
+  server.on("/stop", handleStop);     // 停止运动的请求
   server.begin();
   Serial.println("Web server started");
 }
 
-void loop()
-{
+void loop() {
   server.handleClient();
 }
 
 // 处理GET请求以返回HTML页面
-void handleGet()
-{
-  if (server.method() == HTTP_GET)
-  {
+void handleGet() {
+  if (server.method() == HTTP_GET) {
     server.send(200, "text/html", webpage);
   }
 }
 
 // 处理POST请求以控制电机
-void handlePost()
-{
-  if (server.method() == HTTP_POST)
-  {
+void handlePost() {
+  if (server.method() == HTTP_POST) {
     StaticJsonDocument<200> doc;
     deserializeJson(doc, server.arg("plain"));
     int action = doc["action"].as<int>();
+    bool longPress = doc["longPress"].as<bool>();  // 长按标志
 
-    switch (action)
-    {
-    case 1: // 左转
-      digitalWrite(motorLeftIn1, LOW);
-      digitalWrite(motorLeftIn2, HIGH); // 一个电机向前
-      digitalWrite(motorRightIn1, LOW);
-      digitalWrite(motorRightIn2, LOW); // 另一个电机停止
-      break;
-    case 2: // 右转
-      digitalWrite(motorLeftIn1, LOW);
-      digitalWrite(motorLeftIn2, LOW); // 一个电机停止
-      digitalWrite(motorRightIn1, LOW);
-      digitalWrite(motorRightIn2, HIGH); // 另一个电机向前
-      break;
-    case 3: // 前进是
-      digitalWrite(motorLeftIn1, HIGH);
-      digitalWrite(motorLeftIn2, LOW); // 两个电机都向前
-      digitalWrite(motorRightIn1, HIGH);
-      digitalWrite(motorRightIn2, LOW);
-      break;
-    case 4: // 停止
-      digitalWrite(motorLeftIn1, LOW);
-      digitalWrite(motorLeftIn2, LOW); // 两个电机都停止
-      digitalWrite(motorRightIn1, LOW);
-      digitalWrite(motorRightIn2, LOW);
-      break;
-    case 5: // 后退
-      digitalWrite(motorLeftIn1, LOW);
-      digitalWrite(motorLeftIn2, HIGH); // 两个电机都向后
-      digitalWrite(motorRightIn1, LOW);
-      digitalWrite(motorRightIn2, HIGH);
-      break;
-    default:
-      // 错误处理或忽略非法输入
-      break;
+    setMotorState(action, longPress);
+
+    if (!longPress) {
+      delay(1000);   // 短按持续1秒
+      stopMotors();  // 停止电机
     }
+
     server.send(200, "text/plain", "Action received.");
   }
 }
 
-// ... 其余的HTML和JavaScript代码保持不变 ...
+void setMotorState(int action, bool continuous) {
+  // 根据action设置电机状态
+  switch (action) {
+    case 1:  // 左转
+      digitalWrite(motorLeftIn1, HIGH);
+      digitalWrite(motorLeftIn2, LOW);  // 一个电机向前
+      digitalWrite(motorRightIn1, LOW);
+      digitalWrite(motorRightIn2, HIGH);  // 另一个电机向后
+      break;
+    case 2:  // 右转
+      digitalWrite(motorLeftIn1, LOW);
+      digitalWrite(motorLeftIn2, HIGH);  // 一个电机向后
+      digitalWrite(motorRightIn1, HIGH);
+      digitalWrite(motorRightIn2, LOW);  // 另一个电机向前
+      break;
+    case 3:  // 前进
+      digitalWrite(motorLeftIn1, HIGH);
+      digitalWrite(motorLeftIn2, LOW);  // 两个电机都向前
+      digitalWrite(motorRightIn1, HIGH);
+      digitalWrite(motorRightIn2, LOW);
+      break;
+    case 5:  // 后退
+      digitalWrite(motorLeftIn1, LOW);
+      digitalWrite(motorLeftIn2, HIGH);  // 两个电机都向后
+      digitalWrite(motorRightIn1, LOW);
+      digitalWrite(motorRightIn2, HIGH);
+      break;
+    default:
+      // 如果收到的action不是预期的，停止所有电机
+      stopMotors();
+      break;
+  }
+  // 如果不是长按，不需要在这里停止电机，因为会有单独的停止请求
+}
